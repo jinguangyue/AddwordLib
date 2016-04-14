1907ab3308ddb0cefb8b96b7105720d7cd00caae
yue <1010398694@qq.com>
2016-04-14 10:50:49 +0800

    int commit

---
+++ addworddemo/src/main/java/com/funny/addworddemo/CustomLinearlayout.java
@@ -0,0 +1,73 @@
+package com.funny.addworddemo;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by yue on 2016/4/13.
+ */
+public class CustomLinearlayout extends LinearLayout {
+    private String text;
+    private Context context;
+    private int color;
+    private int size;
+
+    public CustomLinearlayout(Context context) {
+        super(context);
+        setTextViewOrientation(VERTICAL);
+        this.context = context;
+        this.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+    }
+
+    public CustomLinearlayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setTextViewOrientation(VERTICAL);
+        this.context = context;
+        this.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+    }
+
+    public void setTextViewOrientation(int orientation) {
+        setOrientation(orientation);
+    }
+
+    public void setText(String text) {
+        this.text = text;
+        addText();
+    }
+
+    private void addText() {
+        removeAllViews();
+        if (text != null) {
+            char[] chars = text.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                TextView myText = new TextView(context);
+                myText.setTextColor(color);
+                if (size > 0) {
+                    myText.setTextSize(size);
+                }
+                myText.setText(text.substring(i, i + 1));
+                addView(myText);
+            }
+        }
+
+    }
+/*
+
+    public void setMyGravity(int gravity){
+        setGravity(gravity);
+        this.requestLayout();
+    }
+*/
+
+    public void setTextColor(int color) {
+        this.color = color;
+    }
+
+    public void setTextSize(int size) {
+        this.size = size;
+    }
+
+}
---
+++ addworddemo/src/main/java/com/funny/addworddemo/TextVerticalActivity.java
@@ -0,0 +1,83 @@
+package com.funny.addworddemo;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+public class TextVerticalActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private VerticalTextView verticalTextView;
+    private EditText edit_text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_vertical);
+        initView();
+    }
+
+    private void initView() {
+        verticalTextView = (VerticalTextView)findViewById(R.id.verticalTextView);
+        verticalTextView.setTextColor(Color.BLACK);
+        verticalTextView.setTextSize(30);
+        verticalTextView.setTextViewOrientation(LinearLayout.HORIZONTAL);
+        verticalTextView.setText("我就是\n觉得是一个一个的吧");
+
+        findViewById(R.id.hengButton).setOnClickListener(this);
+        findViewById(R.id.shuButton).setOnClickListener(this);
+        findViewById(R.id.zuoduiqi).setOnClickListener(this);
+        findViewById(R.id.juzhong).setOnClickListener(this);
+        findViewById(R.id.youduiqi).setOnClickListener(this);
+        edit_text = (EditText)findViewById(R.id.edit_text);
+        edit_text.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String str = s.toString();
+                if(str != null && !str.equals("")){
+                    verticalTextView.setText(s.toString());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.hengButton:
+                verticalTextView.setTextViewOrientation(LinearLayout.VERTICAL);
+                break;
+
+            case R.id.shuButton:
+                verticalTextView.setTextViewOrientation(LinearLayout.HORIZONTAL);
+                break;
+
+            case R.id.zuoduiqi:
+                verticalTextView.setGravity(Gravity.START);
+                break;
+
+            case R.id.juzhong:
+                verticalTextView.setGravity(Gravity.CENTER);
+                break;
+
+            case R.id.youduiqi:
+                verticalTextView.setGravity(Gravity.END);
+                break;
+        }
+    }
+}
